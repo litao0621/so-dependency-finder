2bdc2fac7fdb53df42decec5131783c6a48a3a10
李涛 <onresume@live.com>
2023-11-10 11:08:41 +0800

    修改部分gradle 配置，调试独立文件中对子项目定义对扩展值获取方式

--- plugin/build.gradle.kts
+++ plugin/build.gradle.kts
@@ -1,9 +1,45 @@
+
+extra["PUBLISH_ARTIFACT_ID"] = "so-dependency-finder"
+
 plugins {
     id("java-library")
     id("org.jetbrains.kotlin.jvm")
+    `kotlin-dsl`
+    `java-gradle-plugin`
 }
 
+val publish_plugin_path = "${rootProject.projectDir}/publish-to-maven-test.gradle.kts"
+if (File(publish_plugin_path).exists()) {
+    apply(from = publish_plugin_path)
+}
+
+
+
+
 java {
-    sourceCompatibility = JavaVersion.VERSION_1_7
-    targetCompatibility = JavaVersion.VERSION_1_7
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
+}
+
+tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
+    kotlinOptions {
+        jvmTarget = JavaVersion.VERSION_17.toString()
+    }
+}
+
+dependencies {
+    compileOnly("com.android.tools.build:gradle:8.1.2")
+    compileOnly("org.jetbrains.kotlin:kotlin-gradle-plugin:1.8.10")
+    compileOnly("com.google.devtools.ksp:com.google.devtools.ksp.gradle.plugin:1.9.10-1.0.13")
+}
+
+
+
+gradlePlugin {
+    plugins {
+        create("SoDependencyFinder") {
+            id = "io.github.litao0621.plugin"
+            implementationClass = "com.litao.lib.plugin.SoDependencyPlugin"
+        }
+    }
 }
--- plugin/src/main/java/com/litao/lib/plugin/SoDependencyPlugin.kt
+++ plugin/src/main/java/com/litao/lib/plugin/SoDependencyPlugin.kt
@@ -1,4 +1,14 @@
 package com.litao.lib.plugin
 
-class SoDependencyPlugin {
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+class SoDependencyPlugin : Plugin<Project> {
+    override fun apply(target: Project) {
+        target.task("soTest"){
+            doLast {
+                println("find .so file dependency")
+            }
+        }
+    }
 }
